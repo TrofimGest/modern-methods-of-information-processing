6a5f427716c1aeb5b147ec4123de3e4e7ca2642b
Ivan <ivanchernetskey@mail.ru>
2022-05-18 23:28:07 +0300

    fix: rename variables

--- random_rotation.py
+++ random_rotation.py
@@ -4,13 +4,13 @@ from random import randint
 def random_rotation(img):
     # получим размеры изображения для поворота
     # и вычислим центр изображения
-    (h,w) = img.shape[:2]
-    center = (w/2, h/2)
+    (height,width) = img.shape[:2]
+    center = (width/2, height/2)
 
     angle = randint(1,359)
 
     # повернем изображение на 180 градусов
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    rotated = cv2.warpAffine(img, M, (w, h))
+    rotated = cv2.warpAffine(img, M, (width, height))
     cv2.imshow("Rotated image", rotated)
     cv2.waitKey(0)
