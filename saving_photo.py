678344529d9dfb2692aba7bf33f64b2f9edc6673
Gnarll <vadimfedorovich22@mail.ru>
2022-05-25 18:27:27 +0300

    refactor saving function to save arrays of photos

--- saving_photo.py
+++ saving_photo.py
@@ -1,18 +1,33 @@
 import cv2
 import os
+from random import randint
 
 
-def saving_photo(img, newImgName, format):
+def saving_photo(photos):
 
     photosPath = './photos'
     isExist = os.path.exists(photosPath)
     if not isExist:
         os.makedirs(photosPath)
 
-    newPath = './photos/' + newImgName + '.' + format
+    for photo in photos:
+        randNum = random_with_N_digits(3)
+        name = 'savedPhoto#' + str(randNum) + '.jpg'
+        while(True):
+            if(nameExists('./photos/' + str(name))):
+                randNum = random_with_N_digits(3)
+                name = 'savedPhoto#' + str(randNum) + '.jpg'
+            else:
+                status = cv2.imwrite('./photos/' + str(name), photo)
+                print("Image written to file-system : ", status)
+                break
 
-    status = cv2.imwrite(newPath, img)
 
-    print("Image written to file-system : ", status)
+def nameExists(name):
+    return os.path.exists(name)
 
-    return status
+
+def random_with_N_digits(n):
+    range_start = 10**(n-1)
+    range_end = (10**n)-1
+    return randint(range_start, range_end)
