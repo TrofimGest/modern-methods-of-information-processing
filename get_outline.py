b02a7ca61b3752e7c34aa6e397b8efc40a094bca
Gnarll <vadimfedorovich22@mail.ru>
2022-05-30 19:31:26 +0300

    refactor getting outline

--- get_outline.py
+++ get_outline.py
@@ -1,21 +1,24 @@
 import cv2
 
+param1 = 100
+param2 = 10
+white = [255, 255, 255]
+thickness = 1
 
-def get_outline(image):
+
+def get_outline(image, blackBackground):
+
     img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(img_gray, 150, 255, cv2.THRESH_BINARY)
-
-    cv2.imshow('Binary image', thresh)
-    cv2.waitKey(0)
+    ret, thresh = cv2.threshold(img_gray, param1, param1, cv2.THRESH_BINARY)
 
     # detect the contours on the binary image
     contours, hierarchy = cv2.findContours(
         thresh, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
 
-    cv2.drawContours(image=image, contours=contours, contourIdx=-1,
-                     color=(0, 0, 255), thickness=2, lineType=cv2.LINE_AA)
+    cv2.drawContours(image=blackBackground, contours=contours, contourIdx=-1,
+                     color=white, thickness=thickness, lineType=cv2.LINE_AA)
 
-    cv2.imshow('Outline', image)
+    cv2.imshow('Outline', blackBackground)
     cv2.waitKey(0)
 
-    return image
+    return blackBackground
